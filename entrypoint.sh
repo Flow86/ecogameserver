97638f42456ce60f43a4cbb49f1ed05560d66cf5
Flow86 <656249+Flow86@users.noreply.github.com>
2020-08-08 14:54:27 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-set -ex
+set -euo pipefail
 
-USER_NAME=docker
-GROUP_NAME=docker
+ECO_UID=$(stat -c '%u' /srv/eco)
+ECO_GID=$(stat -c '%g' /srv/eco)
 
-echo "Running as $UID $GID"
-
-#UID=$(id -u)
-#GID=$(id -g)
-
-# do we have a group name
-if [ -z "$(id -un)" ] ; then
-    groupadd -g $GID $GROUP_NAME
-fi
-
-# do we have a user name?
-if [ -z "$(id -gn)" ] ; then
-    useradd -u $UID -m -s /bin/bash -g $GID $USER_NAME
-fi
-
-exec "$@"
+echo "Calling 'gosu ${ECO_UID}:${ECO_GID} \"$@\"'" >&2
+gosu ${ECO_UID}:${ECO_GID} "$@"
