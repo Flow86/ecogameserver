52bcbb1a8e6b7a47968a58584a55787b2a59eeae
Flow86 <656249+Flow86@users.noreply.github.com>
2020-08-08 14:57:59 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,15 @@ set -euo pipefail
 ECO_UID=$(stat -c '%u' /srv/eco)
 ECO_GID=$(stat -c '%g' /srv/eco)
 
+# do we have a group name
+if [ -z "$(getent group ${ECO_GID})" ] ; then
+    groupadd -g $ECO_GID eco
+fi
+
+# do we have a user name?
+if [ -z "$(getent passwd ${ECO_UID})" ] ; then
+    useradd -u ${ECO_UID} -m -s /bin/bash -g ${ECO_GID} eco
+fi
+
 echo "Calling 'gosu ${ECO_UID}:${ECO_GID} \"$@\"'" >&2
 gosu ${ECO_UID}:${ECO_GID} "$@"
